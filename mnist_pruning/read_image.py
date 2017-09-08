b724f28854303592cb202e93fdab9928d5ac4e7e
nepha <shi.mohan@foxmail.com>
2017-09-08 16:47:32 +0800

    update gitignore

--- mnist_pruning/read_image.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from PIL import Image
-
-def read_image(path):
-    tmp =  np.array(Image.open(path).resize((28, 28), resample=2))
-    img = np.zeros((28, 28, 1))
-    img[:, :, 0] = tmp[:, :, 0]
-    return img
